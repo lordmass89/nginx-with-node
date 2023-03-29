835bbe4dc9b1802323f8596c6b6c4fd8cf8436a9
Maicon Souza <maicon.souza@paipe.co>
2023-03-29 00:47:57 -0300

    Ajustes docker compose e ignores

--- node/index.js
+++ node/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>FullCycle Rocks!!!</h1>')
+  res.send('<h1>FullCycle Rocks!</h1>')
 })
 
 app.listen(port, () => {
